78e7a7ea5bd0ad50d4d0e63dcd518345be4e38b1
neciporhan <neciporhan@gmail.com>
2020-08-23 14:16:08 -0600

    day2 Practice5

--- src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P5_EtsyTitleVerification.java
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P5_EtsyTitleVerification.java
@@ -1,7 +1,10 @@
 package com.cybertek.tests.day2_locators_getText_getAttribute;
 
 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -18,8 +21,25 @@ public class P5_EtsyTitleVerification {
         driver.get("https://www.etsy.com");
 
         //3. Search for “wooden spoon”
+        //find the search box
+        //send keys "wooden spoon"
+
+        driver.findElement(By.id("global-enhancements-search-query")).sendKeys("wooden spoon" + Keys.ENTER);
+
+
         //4. Verify title:
         //Expected: “Wooden spoon | Etsy”
+
+        String expectedTitle = "Wooden spoon | Etsy";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("Etsy title verification PASSED!");
+        }else{
+            System.out.println("Etsy title verification FAILED!!!");
+        }
+
+
 
     }
 
