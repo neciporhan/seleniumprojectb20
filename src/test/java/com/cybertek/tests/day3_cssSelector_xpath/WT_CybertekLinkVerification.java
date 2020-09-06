59b51db343974aa20c91932b593450e5cf38e994
neciporhan <neciporhan@gmail.com>
2020-09-06 13:27:33 -0600

    day3

--- src/test/java/com/cybertek/tests/day3_cssSelector_xpath/WT_CybertekLinkVerification.java
+++ src/test/java/com/cybertek/tests/day3_cssSelector_xpath/WT_CybertekLinkVerification.java
@@ -1,18 +1,19 @@
 package com.cybertek.tests.day3_cssSelector_xpath;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WT_CybertekLinkVerification {
 
     public static void main(String[] args) {
         //TC #3: PracticeCybertek/ForgotPassword URL verification
         //1. Open Chrome browser
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        // WebDriverManager.chromedriver().setup();
+        //WebDriver driver = new ChromeDriver();
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
 
         //2. Go to http://practice.cybertekschool.com/forgot_password
 
@@ -54,7 +55,8 @@ public class WT_CybertekLinkVerification {
 
         System.out.println("actualText = " + actualText);
         System.out.println("expectedText = " + expectedText);
-
+        // mac = command + d for copy/paste one line
+        // windows = cntrl + d
         if (actualText.equals(expectedText)){
             System.out.println("Header text value verification PASSED!");
         }else{
